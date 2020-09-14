1d4edd6e293e0d1d8fd39ed73e14608b1cd55c38
yukitest <syuoo0025@gmail.com>
2020-09-14 16:09:09 +0900

    video chat 文字起こし機能の実装

--- test-pj/laravel/public/js/voice.js
+++ test-pj/laravel/public/js/voice.js
@@ -5,24 +5,28 @@ speech.lang = 'us-US';
 
 //自動スタート
 window.onload = () => (
-  speech.start()
+    speech.start()
 );
 
 //音声自動文字起こし機能
 speech.onresult = function (e) {
-  speech.stop();
-  if (e.results[0].isFinal) {
-    console.log(e.results[0].isFinal);
-      let autotext = e.results[0][0].transcript
-      content.innerHTML += '<div>' + autotext + '</div>';
-  }
+    speech.stop();
+    if (e.results[0].isFinal) {
+        console.log(e.results[0].isFinal);
+        let autotext = e.results[0][0].transcript
+        content.innerHTML += '<h5>' + autotext + '</h5>';         //テキスト出力形式
+        var obj = document.getElementById("content");             //スクロール最下部を指定
+        obj.scrollTop = obj.scrollHeight;
+    }
 };
+
+
   //次の準備
-  speech.onend = () => {
-  speech.start();
-  };
-  speech.onsoundstart = () => {
-  };
-  speech.onsoundend = () => {
+    speech.onend = () => {
+    speech.start();
+    };
+    speech.onsoundstart = () => {
+    };
+    speech.onsoundend = () => {
     // btn.textContent = '発言してください';
-  };
+    };
